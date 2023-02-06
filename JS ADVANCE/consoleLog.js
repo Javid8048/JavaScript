80def6c73d98385804f5151292561c405dbe2f2b
JAVID ASHWAQ F <112797329+Javid8048@users.noreply.github.com>
2023-02-06 17:41:41 +0530

    Add files via upload

---
+++ JS ADVANCE/consoleLog.js
@@ -0,0 +1,5 @@
+var firstNum=10;
+var secondNum=20;
+console.log(firstNum);
+var num1=15;
+console.log(typeof(num1));
---
+++ JS DOM/Script.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    alert("Script Loarder")
+}, 5000); 
+var arr=new Array[1,2,3,4,5];
